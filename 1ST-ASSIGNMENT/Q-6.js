57829e06e601bc7d1cbfd5d183744b314639e323
Sujoy  Karmakar <ksujoy29@gmail.com>
2024-01-03 13:11:29 +0530

    upload 4th nov js fundamental

---
+++ 1ST-ASSIGNMENT/Q-6.js
@@ -0,0 +1,17 @@
+/* Explain the importance of choosing meaningful and descriptive variable names in JavaScript. 
+ Provide an example where using a clear identifier improves code readability.*/
+
+/*
+Ans:- 
+  Choosing meaningful and descriptive variable names in JavaScript is crucial for code readability and maintainability. Clrear identifiers make it easier for both, the original coder or who might read the code later for understand code purpose and functionality of variables.
+
+*/
+// Unclear variable names
+let a = 10;
+let b = 5;
+let c = a + b;
+
+// Clear and descriptive variable names
+let numberOfApples = 10;
+let numberOfOranges = 5;
+let totalFruits = numberOfApples + numberOfOranges;
---
+++ 1ST-ASSIGNMENT/Q5-comments.js
@@ -0,0 +1,27 @@
+/* Question 5: Explain the importance of comments in JavaScript and provide examples of single-line and
+multi-line comments.
+    ans:-
+   Comments in JavaScript are crucial for enhancing code readability and aiding collaboration among Developers.
+   They allow you to embed explations or notes within the code with out affecting it's functionality.   */
+
+//  --------------------single line comment
+
+/* In JavsScript single line comments are created by using two forward slashes. Any thing after this forword slashes will be ignored by
+ the JavaScript interpeter.
+*/
+// This is a sigle line comment 
+
+console.log("hello world");   //This is a single line comment
+
+
+// ---------------------------------------------------------Multi line comment
+
+/*
+In JavaScript multi line comments are start with /* and end with */
+
+// Anything between this symbol will be ignored by the JavaScript interpeter.
+
+/* 
+This is multi line comment 
+*/
+
